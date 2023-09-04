2664fa38963ad3a2d1954cecea8e746376a11325
729149195 <729149195@qq.com>
2023-09-04 17:16:24 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
@@ -14,8 +24,9 @@ export default defineConfig({
     }
   },
   base: '/D3_BuildBy_vue.github.io/',
-  scripts: {
-    "build": "vite build",
-    "preview": "vite preview"
-  }
+  // scripts: {
+  //   "build": "vite build",
+  //   "preview": "vite preview"
+  // }
+
 })
