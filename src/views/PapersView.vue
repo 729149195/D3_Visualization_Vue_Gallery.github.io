ed273464df028035a24929679dd4702dd4fa429b
729149195 <729149195@qq.com>
2023-09-06 23:55:56 +0800

    deploy

--- src/views/PapersView.vue
+++ src/views/PapersView.vue
@@ -5,12 +5,10 @@
 </template>
 
 <script setup>
-  import * as d3 from 'd3'
-  d3.select('.about').append('h3').text('Hello World')
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 @media (min-width: 1024px) {
   .papers {
     min-height: 100vh;
